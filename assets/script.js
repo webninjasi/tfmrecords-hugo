5c88e57d89c17c426571e8a35f3543ac80988b64
webninjasi <webninjasi.0@gmail.com>
2020-11-13 18:36:44 +0300

    Back to proxy!

--- assets/script.js
+++ assets/script.js
@@ -59,7 +59,7 @@ $(function() {
     });
 
     $('img').on("error", function () {
-        if (this.originalSrc || this.src.indexOf('imgur') == -1) {
+        if (this.originalSrc) {
             return;
         }
 
@@ -137,10 +137,12 @@ $(function() {
     if (window.localStorage) {
         if (window.localStorage.getItem('useProxy')) {
             $('img').each(function () {
+                /*
                 if (this.src.indexOf('imgur') == -1)
                 {
                     return;
                 }
+                */
 
                 this.originalSrc = this.src;
                 this.src = 'https://images-docs-opensocial.googleusercontent.com/gadgets/proxy?url=' + encodeURIComponent(this.originalSrc) + '&container=docs&gadget=a&rewriteMime=image%2F*&refresh=86400';
