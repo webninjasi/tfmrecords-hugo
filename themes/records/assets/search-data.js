e47e0b1bcf584cc641df9544ea820d96fe14dd44
webninjasi <webninjasi.0@gmail.com>
2020-12-08 14:05:26 +0300

    Update theme css

--- themes/records/assets/search-data.js
+++ themes/records/assets/search-data.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function() {
+(function () {
   const indexCfg = {{ with i18n "bookSearchConfig" }}
     {{ . }};
   {{ else }}
@@ -10,17 +10,22 @@
   indexCfg.doc = {
     id: 'id',
     field: ['title', 'content'],
-    store: ['title', 'href'],
+    store: ['title', 'href', 'section'],
   };
 
   const index = FlexSearch.create('balance', indexCfg);
   window.bookSearchIndex = index;
 
-  {{ range $index, $page := where .Site.Pages "Kind" "in" (slice "page" "section") }}
+  {{- $pages := where .Site.Pages "Kind" "in" (slice "page" "section") -}}
+  {{- $pages = where $pages "Params.booksearchexclude" "!=" true -}}
+  {{- $pages = where $pages "Content" "not in" (slice nil "") -}}
+
+  {{ range $index, $page := $pages }}
   index.add({
     'id': {{ $index }},
     'href': '{{ $page.RelPermalink }}',
     'title': {{ (partial "docs/title" $page) | jsonify }},
+    'section': {{ (partial "docs/title" $page.Parent) | jsonify }},
     'content': {{ $page.Plain | jsonify }}
   });
   {{- end -}}
